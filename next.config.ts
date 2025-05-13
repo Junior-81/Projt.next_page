adbc1f035818f20a9c317c2b8da6b6fc51fa8355
junior-81 <jab.junior81@gmail.com>
2025-05-13 11:00:59 -0300

    Configuração para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Projt.next_page',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
